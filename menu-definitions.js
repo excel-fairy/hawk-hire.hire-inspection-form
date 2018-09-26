cb46b9a550c71b888726b57fba11b4945379d498
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-09-26 22:40:58 +0200

    oops removed duplicate file

--- menu-definitions.js
+++
@@ -1,7 +0,0 @@
-function onOpen() {
-    var ui = SpreadsheetApp.getUi();
-    ui.createMenu('Run scripts')
-        .addItem('Export ' + PRE_DELIVERY_FORM_SHEET.name, 'exportPreDeliveryForm')
-        .addItem('Export ' + POST_HIRE_FORM_SHEET.name, 'exportPostHireForm')
-        .addToUi();
-}
